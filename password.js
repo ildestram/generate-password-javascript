64b4b8acad8642556ae1c14d533ea4a8a96cba1c
Ilde Stramandinoli <109019707+ildestram@users.noreply.github.com>
2023-01-04 17:44:47 +0100

    ok generatore random

---
+++ password.js
@@ -0,0 +1,23 @@
+let password = document.getElementById("password");
+const letters = "GHBBFGHJGHJBvdhlsjdscmdhd2384749lkw5";
+let passwordLength = 15;
+let passwordProblem = "";
+
+const generatePassword = function(lunghezza){
+    if(lunghezza < 15){
+        alert("password non sicura, riprova");
+        return;
+    }
+    let randomString = '';
+    for(let i=0; i< lunghezza; i++){
+        let randomChar = Math.floor(Math.random() * letters.length);
+        randomString += letters.charAt(randomChar);
+    }
+
+    return randomString;
+}
+
+const randomPassword = generatePassword(15);
+console.log(randomPassword);
+
+document.getElementById("password").value = randomPassword;
